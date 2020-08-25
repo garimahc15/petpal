0877e6b27e03fba62ae24e5ee39a70bf72b6035d
garimahc15 <halcyongarima@gmail.com>
2020-08-26 01:37:54 +0530

    overall design of contact & about page done, removed unnecessary images

--- animals/urls.py
+++ animals/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('about', views.about, name='about'),
     path('dogs', views.dogs, name='dogs'),
     path('cats', views.cats, name='cats'),
